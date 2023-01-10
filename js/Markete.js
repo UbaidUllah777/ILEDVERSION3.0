b6128a41a21ed32b62c2e43da284dba236f891fe
Ubaid <khanxx777@gmail.com>
2023-01-10 12:01:03 +0500

    Ubaid

---
+++ js/Markete.js
@@ -0,0 +1,230 @@
+const ourMarkets=[
+
+// Restaurant
+{
+        id:'idRestaurant',
+        marketeHeading:'Restaurant',
+        marketePara:'ILED screens increase a restaurant’s visibility in a saturated marketplace.  Full and quick service establishments see reduced labor costs and improved image quality with their marketing messaging with the use of digital compared to static letter change reader boards.  Immediate messaging can maximize restaurant owner’s profits with real time specials and impulse purchases from effective advertising content on their digital display.  NEXT software allows for flexible messaging based on the hour, day, week, or month of the year.',
+        marketeImgForIndex:'',
+        marketeImagesForMarketePage:[
+            {Image:''},{Image:''},{Image:''},
+        ]
+
+},
+
+// Medical
+{
+    id:'idMedical',
+    marketeHeading:'Medical',
+    marketePara:'The healthcare industry benefits from clear communication.  Patients and staff can stay up to date with important information that can lead to expedited and improved care.  Marketing services, products, and wait times engages patients in a positive way, that results in improved outcomes and quality of life.  LED Displays allow for facilities to stand out among the clutter and reach customers more effectively.',
+    marketeImgForIndex:'',
+    marketeImagesForMarketePage:[
+        {Image:''},{Image:''},{Image:''},
+    ]
+
+},
+
+
+// Automotive
+{
+    id:'idAutomotive',
+    marketeHeading:'Automotive',
+    marketePara:'The idea of your own marketing billboard outside of your automotive establishment is amazing.  And it has never been more affordable.  The CPM (cost per thousand impressions) with digital signs is the lowest available form of advertising available to business owners.  Paired with the flexibility of messaging options, LED displays provide quick service tools to promote all campaigns as well as specific product and services.',
+    marketeImgForIndex:'',
+    marketeImagesForMarketePage:[
+        {Image:''},{Image:''},{Image:''},
+    ]
+
+},
+
+// Banking
+{
+    id:'idBanking',
+    marketeHeading:'Banking',
+    marketePara:'Banks and credit unions can leverage LED display technology to maximize communication with their current and potential clients.  NEXT displays can dynamically update interest rates, commodities, and stock market indices in a way that will make your institution stand out and rise above the crowd.  Increase foot traffic and boost profit with easy to use software and an exceptional warranty.',
+    marketeImgForIndex:'',
+    marketeImagesForMarketePage:[
+        {Image:''},{Image:''},{Image:''},
+    ]
+
+},
+
+// Retail
+{
+    id:'idRetail',
+    marketeHeading:'Retail',
+    marketePara:'The retail industry has seen significant change over the past decade.  The increased purchasing options and marketing channels has made the effectiveness and cost difficult to digest.  On premise LED digital displays provide an increased opportunity, at a more affordable price, to reach customers with a high frequency tool designed to promote impulse stops and important product and price advantages.  Increase foot traffic with engaging content and up to date specials and offers.',
+    marketeImgForIndex:'',
+    marketeImagesForMarketePage:[
+        {Image:''},{Image:''},{Image:''},
+    ]
+
+},
+
+// Religious
+{
+    id:'idReligious',
+    marketeHeading:'Religious',
+    marketePara:'Exterior and interior LED Displays are a great way for religious organizations to raise awareness in the community and increase membership.  An outdoor monument sign provides high frequency communication; Service times, special events, weddings, community events, welcoming, volunteer recognition.  Easy to use software and content creation tools make it easy for anyone on the staff to develop and deploy quick important messaging.',
+    marketeImgForIndex:'',
+    marketeImagesForMarketePage:[
+        {Image:''},{Image:''},{Image:''},
+    ]
+
+},
+
+// Legal
+{
+    id:'idLegal',
+    marketeHeading:'Legal',
+    marketePara:'85% of customers live within a five-mile radius of your business.  LED displays provide a high-frequency tool that promotes your services to the community.  Legal providers find huge success with top of mind awareness communication.  When potential clients find a need for your service, the work of Digital displays brand awareness over time can drive profitability.',
+    marketeImgForIndex:'',
+    marketeImagesForMarketePage:[
+        {Image:''},{Image:''},{Image:''},
+    ]
+
+},
+
+// Entertainment
+{
+    id:'idEntertainment',
+    marketeHeading:'Entertainment',
+    marketePara:'Interior and exterior digital signs can bring any venue into the 21st century.  On premise outdoor, outdoor wall signs, interior LED and LCD displays, lobby spectaculars, and digital scoreboards and information displays provide guests, clients, fans, and staff opportunities to engage with the complete experience.',
+    marketeImgForIndex:'',
+    marketeImagesForMarketePage:[
+        {Image:''},{Image:''},{Image:''},
+    ]
+
+},
+
+// Sports
+{
+    id:'idSports',
+    marketeHeading:'Sports',
+    marketePara:'Interior and exterior digital signs can bring any sporting venue into the 21st century.  On-premise outdoor, outdoor wall signs, interior LED and LCD displays, lobby spectaculars, and digital scoreboards and information displays provide guests, clients, fans, and staff opportunities to engage with the complete experience.',
+    marketeImgForIndex:'',
+    marketeImagesForMarketePage:[
+        {Image:''},{Image:''},{Image:''},
+    ]
+
+},
+
+
+
+]
+  
+  
+// var index = 0;
+// setInterval(function () {
+//   if (index === (ourMarkets.length)) {
+//     index = 0;
+//   }
+//   else{
+//     for (const singleMarkete of ourMarkets){
+//    if(singleMarkete.){}
+//             console.log(singleMarkete.marketeHeading)
+
+//     }
+//   index++;
+// }
+
+  
+// }, 7000);
+
+
+
+// while (true) {
+//     for (let i = 0; i < ourMarkets.length; i++) {
+//         setTimeout(() => {
+//         console.log(ourMarkets[i]);
+//     }, 10000 * i);
+//     }
+    
+// }
+
+// const FnLoadMarkete=()=>{
+//     for (let i = 0; i < ourMarkets.length; i++) {
+//         if(i < ourMarkets.length){
+//             setTimeout(() => {
+        
+//                 const ELidIndexIndustryHeading =document.getElementById("idIndexIndustryHeading");
+//                 if(ELidIndexIndustryHeading){
+//                  ELidIndexIndustryHeading.textContent = ourMarkets[i].marketeHeading;
+//                 }
+//                  const ElidInexindustryImage=document.getElementById("idInexindustryImage");
+//                  if(ElidInexindustryImage){
+//                      ElidInexindustryImage.src = ourMarkets[i].marketeImgForIndex;
+//                  }
+//                  const ElidIndexIndustryPara=document.getElementById("idIndexIndustryPara");
+//                  if(ElidIndexIndustryPara){
+//                      ElidIndexIndustryPara.textContent = ourMarkets[i].marketePara;
+//                  }
+            
+            
+            
+//               }, 2000 * i);
+//         }
+        
+     
+//     }
+  
+        
+  
+// }
+
+
+
+
+
+  
+  
+  var marketeHeadings = [
+    "Entertainment & Concerts",
+    "Sports",
+    "Venue",
+    "Retail",
+    "Restaurant",
+  ];
+  var marketeParas = [
+    "Pariatur reprehenderit mollit aliqua deserunt aliquip sunt aliquip veniam. Duis laboris ex pariatur nostrud culpa. Adipisicing esse id anim nulla non veniam nisi velit. Ullamco dolore cupidatat sit reprehenderit ut mollit. Ea id laboris est nisi ad cillum proident sit nostrud sunt ullamco labore. Sunt culpa amet consequat ipsum in irure.",
+    "Adipisicing laboris fugiat non incididunt enim Lorem voluptate fugiat fugiat laborum. Incididunt laboris sunt sunt anim non do occaecat anim. Duis in laboris eu ipsum deserunt nulla enim ex commodo deserunt adipisicing minim. Quis tempor veniam dolor voluptate officia et quis officia mollit mollit. Labore nulla elit non est aliqua officia ad ad velit nulla aliqua.",
+    "Pariatur esse laborum sunt laboris Lorem elit labore esse cillum eiusmod excepteur ea. Eiusmod dolore eiusmod culpa magna reprehenderit tempor Lorem laboris amet ex non minim. Ea exercitation proident anim aute enim minim incididunt adipisicing tempor occaecat aliqua adipisicing. Nulla duis culpa consectetur incididunt eiusmod officia deserunt tempor incididunt id minim duis consectetur..",
+    "Cillum nisi exercitation eiusmod cillum Lorem et elit velit aliqua et. Consequat aliquip proident excepteur pariatur velit velit. Qui consequat aliqua laborum ex voluptate aliqua. Occaecat aliquip proident reprehenderit excepteur adipisicing eu id dolore eu aliqua laboris laboris dolore. Reprehenderit do consectetur incididunt cupidatat culpa enim ullamco sunt.",
+    "Anim proident adipisicing commodo culpa tempor velit labore dolore ipsum officia sunt mollit. Aliqua eu eiusmod officia ea ut eu enim cupidatat. Nulla cupidatat pariatur tempor duis nulla exercitation sunt laboris. Culpa ad cupidatat cupidatat id Lorem labore officia occaecat proident aliqua aliquip consequat enim labore. Duis nostrud ex deserunt quis aute exercitation sit ut exercitation incididunt aute ad qui. Sint anim esse mollit ea voluptate voluptate nisi eu sint ut exercitation aliquip labore officia.",
+  ];
+  var indexMaerketeImg = [
+    "img/demos/iLEDv3/marketes/Church-Rental.jpg",
+    "img/demos/iLEDv3/marketes/Ice hockey - Scoreboard.jpg",
+    "img/demos/iLEDv3/marketes/iledscreen - indoor categories.jpg",
+    "img/demos/iLEDv3/marketes/LCD-digital-signage.jpg",
+    "img/demos/iLEDv3/marketes/stage-show.jpg",
+  ];
+
+  var index = 0;
+//   setInterval(function () {
+//     if (index === marketeHeadings.length) {
+//       index = 0;
+//     }
+
+
+
+//     // MARKETS IN INDEX PAGE:START
+//    const ELidIndexIndustryHeading =document.getElementById("idIndexIndustryHeading");
+//    if(ELidIndexIndustryHeading){
+//     ELidIndexIndustryHeading.textContent = marketeHeadings[index];
+//    }
+//     const ElidInexindustryImage=document.getElementById("idInexindustryImage");
+//     if(ElidInexindustryImage){
+//         ElidInexindustryImage.src = indexMaerketeImg[index]
+//     }
+//     const ElidIndexIndustryPara=document.getElementById("idIndexIndustryPara");
+//     if(ElidIndexIndustryPara){
+//         ElidIndexIndustryPara.textContent = marketeParas[index];
+//     }
+   
+    
+//     index++;
+//   }, 7000);
+   // MARKETS IN INDEX PAGE:END
+
+
