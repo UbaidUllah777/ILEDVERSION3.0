a08eedce5e213548a15ea8315adb32be24489002
Ubaid <khanxx777@gmail.com>
2023-01-11 13:36:29 +0500

    Ubaid

--- js/Markete.js
+++ js/Markete.js
@@ -7,7 +7,9 @@ const ourMarkets=[
         marketePara:'ILED screens increase a restaurant’s visibility in a saturated marketplace.  Full and quick service establishments see reduced labor costs and improved image quality with their marketing messaging with the use of digital compared to static letter change reader boards.  Immediate messaging can maximize restaurant owner’s profits with real time specials and impulse purchases from effective advertising content on their digital display.  NEXT software allows for flexible messaging based on the hour, day, week, or month of the year.',
         marketeImgForIndex:'img/demos/iLEDv3/marketes/for index/Restaurant.jpg',
         marketeImagesForMarketePage:[
-            {Image:''},{Image:''},{Image:''},
+            {Image:'img/demos/iLEDv3/marketes/for index/Restaurant.jpg'},
+            {Image:'img/demos/iLEDv3/marketes/for index/Restaurant.jpg'},
+            {Image:'img/demos/iLEDv3/marketes/for index/Restaurant.jpg'},
         ]
 
 },
@@ -19,7 +21,9 @@ const ourMarkets=[
     marketePara:'The healthcare industry benefits from clear communication.  Patients and staff can stay up to date with important information that can lead to expedited and improved care.  Marketing services, products, and wait times engages patients in a positive way, that results in improved outcomes and quality of life.  LED Displays allow for facilities to stand out among the clutter and reach customers more effectively.',
     marketeImgForIndex:'img/demos/iLEDv3/marketes/for index/Medical.jpg',
     marketeImagesForMarketePage:[
-        {Image:''},{Image:''},{Image:''},
+        {Image:'img/demos/iLEDv3/marketes/for index/Medical.jpg'},
+        {Image:'img/demos/iLEDv3/marketes/for index/Medical.jpg'},
+        {Image:'img/demos/iLEDv3/marketes/for index/Medical.jpg'},
     ]
 
 },
@@ -32,7 +36,9 @@ const ourMarkets=[
     marketePara:'The idea of your own marketing billboard outside of your automotive establishment is amazing.  And it has never been more affordable.  The CPM (cost per thousand impressions) with digital signs is the lowest available form of advertising available to business owners.  Paired with the flexibility of messaging options, LED displays provide quick service tools to promote all campaigns as well as specific product and services.',
     marketeImgForIndex:'img/demos/iLEDv3/marketes/for index/Automative.jpg',
     marketeImagesForMarketePage:[
-        {Image:''},{Image:''},{Image:''},
+        {Image:'img/demos/iLEDv3/marketes/for index/Automative.jpg'},
+        {Image:'img/demos/iLEDv3/marketes/for index/Automative.jpg'},
+        {Image:'img/demos/iLEDv3/marketes/for index/Automative.jpg'},
     ]
 
 },
@@ -44,7 +50,9 @@ const ourMarkets=[
     marketePara:'Banks and credit unions can leverage LED display technology to maximize communication with their current and potential clients.  NEXT displays can dynamically update interest rates, commodities, and stock market indices in a way that will make your institution stand out and rise above the crowd.  Increase foot traffic and boost profit with easy to use software and an exceptional warranty.',
     marketeImgForIndex:'img/demos/iLEDv3/marketes/for index/Banking.jpg',
     marketeImagesForMarketePage:[
-        {Image:''},{Image:''},{Image:''},
+        {Image:'img/demos/iLEDv3/marketes/for index/Banking.jpg'},
+        {Image:'img/demos/iLEDv3/marketes/for index/Banking.jpg'},
+        {Image:'img/demos/iLEDv3/marketes/for index/Banking.jpg'},
     ]
 
 },
@@ -56,7 +64,9 @@ const ourMarkets=[
     marketePara:'The retail industry has seen significant change over the past decade.  The increased purchasing options and marketing channels has made the effectiveness and cost difficult to digest.  On premise LED digital displays provide an increased opportunity, at a more affordable price, to reach customers with a high frequency tool designed to promote impulse stops and important product and price advantages.  Increase foot traffic with engaging content and up to date specials and offers.',
     marketeImgForIndex:'img/demos/iLEDv3/marketes/for index/Retail.jpg',
     marketeImagesForMarketePage:[
-        {Image:''},{Image:''},{Image:''},
+        {Image:'img/demos/iLEDv3/marketes/for index/Retail.jpg'},
+        {Image:'img/demos/iLEDv3/marketes/for index/Retail.jpg'},
+        {Image:'img/demos/iLEDv3/marketes/for index/Retail.jpg'},
     ]
 
 },
@@ -65,10 +75,12 @@ const ourMarkets=[
 {
     id:'idReligious',
     marketeHeading:'Religious',
-    marketePara:'Exterior and interior LED Displays are a great way for religious organizations to raise awareness in the community and increase membership.  An outdoor monument sign provides high frequency communication; Service times, special events, weddings, community events, welcoming, volunteer recognition.  Easy to use software and content creation tools make it easy for anyone on the staff to develop and deploy quick important messaging.',
+    marketePara:'Exterior and interior LED Displays are a great way for religious organizations to raise awareness in the community and increase membership.  An outdoor monument sign provides high frequency communication; Service times, special events, weddings, community events, welcoming, volunteer recognition.  Easy to use software and content creation tools make it easy for any${accCount} on the staff to develop and deploy quick important messaging.',
     marketeImgForIndex:'img/demos/iLEDv3/marketes/for index/Religious.jpg',
     marketeImagesForMarketePage:[
-        {Image:''},{Image:''},{Image:''},
+        {Image:'img/demos/iLEDv3/marketes/for index/Religious.jpg'},
+        {Image:'img/demos/iLEDv3/marketes/for index/Religious.jpg'},
+        {Image:'img/demos/iLEDv3/marketes/for index/Religious.jpg'},
     ]
 
 },
@@ -77,10 +89,12 @@ const ourMarkets=[
 {
     id:'idLegal',
     marketeHeading:'Legal',
-    marketePara:'85% of customers live within a five-mile radius of your business.  LED displays provide a high-frequency tool that promotes your services to the community.  Legal providers find huge success with top of mind awareness communication.  When potential clients find a need for your service, the work of Digital displays brand awareness over time can drive profitability.',
+    marketePara:'85% of customers live within a ${accCount}-mile radius of your business.  LED displays provide a high-frequency tool that promotes your services to the community.  Legal providers find huge success with top of mind awareness communication.  When potential clients find a need for your service, the work of Digital displays brand awareness over time can drive profitability.',
     marketeImgForIndex:'img/demos/iLEDv3/marketes/for index/Legal.jpg',
     marketeImagesForMarketePage:[
-        {Image:''},{Image:''},{Image:''},
+        {Image:'img/demos/iLEDv3/marketes/for index/Legal.jpg'},
+        {Image:'img/demos/iLEDv3/marketes/for index/Legal.jpg'},
+        {Image:'img/demos/iLEDv3/marketes/for index/Legal.jpg'},
     ]
 
 },
@@ -92,7 +106,9 @@ const ourMarkets=[
     marketePara:'Interior and exterior digital signs can bring any venue into the 21st century.  On premise outdoor, outdoor wall signs, interior LED and LCD displays, lobby spectaculars, and digital scoreboards and information displays provide guests, clients, fans, and staff opportunities to engage with the complete experience.',
     marketeImgForIndex:'img/demos/iLEDv3/marketes/for index/Entertainment.jpg',
     marketeImagesForMarketePage:[
-        {Image:''},{Image:''},{Image:''},
+        {Image:'img/demos/iLEDv3/marketes/for index/Entertainment.jpg'},
+        {Image:'img/demos/iLEDv3/marketes/for index/Entertainment.jpg'},
+        {Image:'img/demos/iLEDv3/marketes/for index/Entertainment.jpg'},
     ]
 
 },
@@ -104,7 +120,9 @@ const ourMarkets=[
     marketePara:'Interior and exterior digital signs can bring any sporting venue into the 21st century.  On-premise outdoor, outdoor wall signs, interior LED and LCD displays, lobby spectaculars, and digital scoreboards and information displays provide guests, clients, fans, and staff opportunities to engage with the complete experience.',
     marketeImgForIndex:'img/demos/iLEDv3/marketes/for index/Sports.jpg',
     marketeImagesForMarketePage:[
-        {Image:''},{Image:''},{Image:''},
+        {Image:'img/demos/iLEDv3/marketes/for index/Sports.jpg'},
+        {Image:'img/demos/iLEDv3/marketes/for index/Sports.jpg'},
+        {Image:'img/demos/iLEDv3/marketes/for index/Sports.jpg'},
     ]
 
 },
@@ -114,6 +132,68 @@ const ourMarkets=[
 ]
   
   
+
+
+
+
+// Loading Markete Data In Mrkete Page: START
+
+const ELaccordion100=document.getElementById("accordion100");
+let accCount=1
+for(const singleMarkete of ourMarkets){
+    if(ELaccordion100){
+        ELaccordion100.innerHTML+=`
+        <div class="card card-default">
+        <div class="card-header" id="collapse100Heading${accCount}">
+          <h4 class="card-title m-0">
+            <a class="accordion-toggle text-color-dark font-weight-bold collapsed" data-bs-toggle="collapse" data-bs-target="#collapse100${accCount}" aria-expanded="false" aria-controls="collapse100${accCount}">
+              ${singleMarkete.marketeHeading}
+            </a>
+          </h4>
+        </div>
+        <div id="collapse100${accCount}" class="collapse" aria-labelledby="collapse1Heading${accCount}" data-bs-parent="#accordion100" style="">
+          <div class="card-body">
+            <div class="row justify-content-center">
+            <div class="col-lg-7 col-xl-6 pe-lg-4 mb-4 mb-lg-0" >
+            <div style="opacity:1" id="marketeImages${accCount}" class="owl-carousel stage-margin stage-margin-sm nav-style-1 nav-svg-arrows-1 nav-dark custom-stage-outer-box-shadow" data-plugin-options="{'responsive': {'0': {'items': 1}, '476': {'items': 1}, '768': {'items': 1}, '992': {'items': 1}, '1200': {'items': 1}}, 'autoplay': true, 'autoplayTimeout': 1000, 'autoplayHoverPause': false, 'dots': true, 'nav': true, 'loop': true, 'margin': 20, 'stagePadding': 50}">
+             
+            
+            </div>
+            
+          </div>
+              <div class="col-lg-5 col-xl-6">
+                <p class="">${singleMarkete.marketePara} </p>
+                
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+        
+        `;
+
+
+        const ElmarketeImages=document.getElementById(`marketeImages${accCount}`);
+        for (const singleMarketeImage of singleMarkete.marketeImagesForMarketePage){
+            if(ElmarketeImages){
+              ElmarketeImages.innerHTML+=`
+              <div>
+              <img src="${singleMarketeImage.Image}" class="img-fluid" alt="" />
+            </div>
+              `;
+            }
+
+        }
+        accCount++;
+    }
+}
+
+
+// Loading Markete Data In Mrkete Page: END
+
+
+
+
 
 // Loading Markete Data in Index Page : START
 let i = 0;
@@ -135,10 +215,7 @@ const printMarkets = setInterval(() => {
         i = 0;
     }
 }, 5000);
-
-
 // Loading Markete Data in Index Page : END
-
 
 
  
